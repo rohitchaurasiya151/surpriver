988a25317079fb0f9608a827847f517b17cddabb
Rohit Chaurasiya <rohitchaursiya7383@gmail.com>
2026-06-19 16:12:03 +0530

    fix: update deprecated matplotlib style and add stock symbol reference file

--- detection_engine.py
+++ detection_engine.py
@@ -23,7 +23,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Styling for plots
-plt.style.use('seaborn-white')
+plt.style.use('seaborn-v0_8-white')
 plt.rc('grid', linestyle="dotted", color='#a0a0a0')
 plt.rcParams['axes.edgecolor'] = "#04383F"
 
